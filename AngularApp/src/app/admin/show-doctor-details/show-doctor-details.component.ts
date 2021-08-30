06b71907bf804380fe09a6e277ee8bfbdb3ddd9e
DeshanHarshana <nawarathnadeshan@gmail.com>
2021-08-30 10:15:11 +0530

    change routes

---
+++ AngularApp/src/app/admin/show-doctor-details/show-doctor-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-doctor-details',
+  templateUrl: './show-doctor-details.component.html',
+  styleUrls: ['./show-doctor-details.component.css']
+})
+export class ShowDoctorDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

46cf2411b8be71bff87b599b26a10c962fe8602c
ShalikaNawarathna <shalikachathuranganii22@gmail.com>
2021-09-20 13:53:32 +0530

    show report

--- AngularApp/src/app/patient/add-report/add-report.component.ts
+++ AngularApp/src/app/patient/add-report/add-report.component.ts
@@ -64,11 +64,11 @@ export class AddReportComponent implements OnInit {
 
   addReport(report:any){
     report.date=this.methods.convert(report.date);
-     console.log(report);
+     //console.log(report);
 
-     /*this.reportS.addReport(report).subscribe(res=>{
+     this.reportS.addReport(report).subscribe(res=>{
        console.log(res);
-     })*/
+     })
     }
 
 }
